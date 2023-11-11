bd7995e78965e3fa3411fdd6d75619fbd86f976c
Alejandro Padilla <alejopadilla297@gmail.com>
2023-11-10 22:46:45 -0500

    fix upperCase file Typography

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import Typography from "./typography";
+import Typography from "./Typography";
 import { useState } from "react";
-
 
 const Navbar = () => {
   const [selectedLink, setSelectedLink] = useState(null);
@@ -33,9 +32,19 @@ const Navbar = () => {
           <div className="navbar-list">
             <span className="navbar-item">
               <div className="menu-item">
-                <Link href="#" className="buttom12" onClick={() => handleLinkClick("FUECHY")}>
+                <Link
+                  href="#"
+                  className="buttom12"
+                  onClick={() => handleLinkClick("FUECHY")}
+                >
                   FUECHY{" "}
-                  <img className="hola" src="/flecha.svg" alt="Descripción del SVG" width="16" height="16" />
+                  <img
+                    className="hola"
+                    src="/flecha.svg"
+                    alt="Descripción del SVG"
+                    width="16"
+                    height="16"
+                  />
                 </Link>
                 <ul className="sub-menu">
                   <li>
@@ -114,31 +123,84 @@ const Navbar = () => {
           <div className={`bar ${showMenu ? "change" : ""}`} />
           <div className={`bar ${showMenu ? "change" : ""}`} />
         </div>
-{showMenu && (
-  <div className="modal-container">
-    <div className="modal">
-      <div className="submenu">
-        {/* Opciones principales */}
-        <Link href="#" className={`navbar-link ${selectedLink === "FUECHY" ? "selected" : ""}`} onClick={() => handleLinkClick("FUECHY")}>FUECHY</Link>
-        <Link href="/about" className={`navbar-link ${selectedLink === "SOBRE EL PROYECTO" ? "selected" : ""}`} onClick={() => handleLinkClick("SOBRE EL PROYECTO")}>SOBRE EL PROYECTO</Link>
-        <Link href="/credits" className={`navbar-link ${selectedLink === "CRÉDITOS" ? "selected" : ""}`} onClick={() => handleLinkClick("CRÉDITOS")}>CRÉDITOS</Link>
-        
-        {/* Opciones secundarias */}
-        {selectedLink === "FUECHY" && (
-          <>
-            <Link href="/ubasucac" className={`navbar-link ${selectedLink === "UBASUCAC" ? "selected" : ""}`} onClick={() => handleLinkClick("UBASUCAC")}>UBASUCAC AGUENÉ</Link>
-            <Link href="/chicubun" className={`navbar-link ${selectedLink === "CHICUBUN" ? "selected" : ""}`} onClick={() => handleLinkClick("CHICUBUN")}>CHICUBUN</Link>
-            <Link href="/quyca" className={`navbar-link ${selectedLink === "QUYCA" ? "selected" : ""}`} onClick={() => handleLinkClick("QUYCA")}>QUYCA</Link>
-            <Link href="/hizqa" className={`navbar-link ${selectedLink === "HIZQA" ? "selected" : ""}`} onClick={() => handleLinkClick("HIZQA")}>HIZQA SAASBISA</Link>
-          </>
+        {showMenu && (
+          <div className="modal-container">
+            <div className="modal">
+              <div className="submenu">
+                {/* Opciones principales */}
+                <Link
+                  href="#"
+                  className={`navbar-link ${
+                    selectedLink === "FUECHY" ? "selected" : ""
+                  }`}
+                  onClick={() => handleLinkClick("FUECHY")}
+                >
+                  FUECHY
+                </Link>
+                <Link
+                  href="/about"
+                  className={`navbar-link ${
+                    selectedLink === "SOBRE EL PROYECTO" ? "selected" : ""
+                  }`}
+                  onClick={() => handleLinkClick("SOBRE EL PROYECTO")}
+                >
+                  SOBRE EL PROYECTO
+                </Link>
+                <Link
+                  href="/credits"
+                  className={`navbar-link ${
+                    selectedLink === "CRÉDITOS" ? "selected" : ""
+                  }`}
+                  onClick={() => handleLinkClick("CRÉDITOS")}
+                >
+                  CRÉDITOS
+                </Link>
+
+                {/* Opciones secundarias */}
+                {selectedLink === "FUECHY" && (
+                  <>
+                    <Link
+                      href="/ubasucac"
+                      className={`navbar-link ${
+                        selectedLink === "UBASUCAC" ? "selected" : ""
+                      }`}
+                      onClick={() => handleLinkClick("UBASUCAC")}
+                    >
+                      UBASUCAC AGUENÉ
+                    </Link>
+                    <Link
+                      href="/chicubun"
+                      className={`navbar-link ${
+                        selectedLink === "CHICUBUN" ? "selected" : ""
+                      }`}
+                      onClick={() => handleLinkClick("CHICUBUN")}
+                    >
+                      CHICUBUN
+                    </Link>
+                    <Link
+                      href="/quyca"
+                      className={`navbar-link ${
+                        selectedLink === "QUYCA" ? "selected" : ""
+                      }`}
+                      onClick={() => handleLinkClick("QUYCA")}
+                    >
+                      QUYCA
+                    </Link>
+                    <Link
+                      href="/hizqa"
+                      className={`navbar-link ${
+                        selectedLink === "HIZQA" ? "selected" : ""
+                      }`}
+                      onClick={() => handleLinkClick("HIZQA")}
+                    >
+                      HIZQA SAASBISA
+                    </Link>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
         )}
-      </div>
-    </div>
-  </div>
-)}
-
-
-
       </div>
     </nav>
   );
