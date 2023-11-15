8c6b1e896a4edde11a8c0e32d0f3bf975c36f616
Alejandro Padilla <alejopadilla297@gmail.com>
2023-11-15 00:20:11 -0500

    audios hizqa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/ImageMixer.css";
+import "@/styles/ImageMixerHizqa.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
